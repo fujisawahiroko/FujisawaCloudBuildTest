b556b2202626ad2c0edecdcaa2ac646900e4982f
fujisawahiroko <hiroko-fujisawa@tayutau.co.jp>
2016-12-09 18:52:39 +0900

    更新

--- JenkinsBuildTest/Assets/Editor/BuildTool.cs
+++ JenkinsBuildTest/Assets/Editor/BuildTool.cs
@@ -7,20 +7,21 @@ public class BuildTool {
   // ビルド実行でAndroidのapkを作成する
   [UnityEditor.MenuItem("Tools/Build/Android")]
   public static void BuildAndroid() {
-	EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
-	List<string> allScene = new List<string>();
-	foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
-	  if (scene.enabled) {
-		allScene.Add (scene.path);
-	  }
-	}   
-	PlayerSettings.bundleIdentifier = "com.tayutau";
-	PlayerSettings.statusBarHidden = true;
-	BuildPipeline.BuildPlayer( 
-	  allScene.ToArray(),
-	  "newgame.apk",
-	  BuildTarget.Android,
-	  BuildOptions.None
-	);
+//	EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+//	List<string> allScene = new List<string>();
+//	foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+//	  if (scene.enabled) {
+//		allScene.Add (scene.path);
+//	  }
+//	}   
+//	PlayerSettings.bundleIdentifier = "com.tayutau";
+//	PlayerSettings.statusBarHidden = true;
+//	BuildPipeline.BuildPlayer( 
+//	  allScene.ToArray(),
+//	  "newgame.apk",
+//	  BuildTarget.Android,
+//	  BuildOptions.None
+//	);
+	Debug.Log("実行");
   }
 }
