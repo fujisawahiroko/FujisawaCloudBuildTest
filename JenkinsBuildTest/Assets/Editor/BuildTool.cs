3428272079450e8549327343bf7a3a657a63c3f8
fujisawahiroko <hiroko-fujisawa@tayutau.co.jp>
2016-12-09 11:21:10 +0900

    プロジェクト追加

---
+++ JenkinsBuildTest/Assets/Editor/BuildTool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class BuildTool {
+  // ビルド実行でAndroidのapkを作成する
+  [UnityEditor.MenuItem("Tools/Build/Android")]
+  public static void BuildAndroid() {
+	EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+	List<string> allScene = new List<string>();
+	foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+	  if (scene.enabled) {
+		allScene.Add (scene.path);
+	  }
+	}   
+	PlayerSettings.bundleIdentifier = "com.tayutau";
+	PlayerSettings.statusBarHidden = true;
+	BuildPipeline.BuildPlayer( 
+	  allScene.ToArray(),
+	  "newgame.apk",
+	  BuildTarget.Android,
+	  BuildOptions.None
+	);
+  }
+}
